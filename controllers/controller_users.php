eb21036be6ed51e4dad70bb64fac3ee16ebad849
albertoornelasdev <albertoornelasdev@gmail.com>
2022-01-05 16:02:58 -0700

    signup done

--- controllers/controller_users.php
+++ controllers/controller_users.php
@@ -7,17 +7,21 @@ DB::createInstance();
 
 class UsersController {
     public function login(){
-
-        if ($_POST) {
-            print_r($_POST);
-        }
-
         include_once("views/pages/login.php");
     }
 
     public function signup(){
         if ($_POST) {
-            print_r($_POST);
+            $fullname = $_POST['fullname'];
+            $email = $_POST['email'];
+            $encryptedPwd = md5($_POST['password']);
+            $birthDate = date("Y-m-d", strtotime($_POST['birthDate']));;
+
+            if ( ($fullname == "") || ($email == "") || ($encryptedPwd == "") || ($birthDate == "")) {
+                echo "All fileds are required, please fill the form";
+            } else {
+                User::signUpProcess($fullname, $email, $encryptedPwd, $birthDate);
+            }
         }
         include_once("views/pages/signup.php");
     }
--- models/user.php
+++ models/user.php
@@ -3,24 +3,29 @@ class User{
     public $id;
     public $fullname;
     public $email;
-    public $pass;
+    public $password;
     public $birthDate;
 
     // constructor
-    public function __construct($id, $fullname, $email, $pass, $birthDate) {
+    public function __construct($id, $fullname, $email, $password, $birthDate) {
         $this->id = $id;
         $this->fullname = $fullname;
         $this->email = $email;
-        $this->pass= $pass;
+        $this->password= $password;
         $this->birthDate = $birthDate;
     }
 
-    public static function loginProcess($email, $pass) {
-        
+    public static function loginProcess($email, $password) {
     }
 
-    public static function signUpProcess($email, $pass) {
-        
+    public static function signUpProcess($fullname, $email, $password, $birthDate) {
+        $connectionDB = DB::createInstance();
+        $sql = $connectionDB->prepare("INSERT INTO users(fullname, email, password, birthDate) VALUES (?,?,?,?)");
+        if ($sql->execute(array($fullname, $email, $password, $birthDate))) {
+            echo "Account created successful";
+        } else {
+            echo "Something wrong!";
+        }
     }
 }
 
