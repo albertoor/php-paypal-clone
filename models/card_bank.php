55b2002b08c44c20dd5e384238d6403ee4799352
albertoornelasdev <albertoornelasdev@gmail.com>
2022-01-07 00:12:03 -0700

    card bank model

---
+++ models/card_bank.php
@@ -0,0 +1,24 @@
+<?php
+class CardBank {
+    public $id_card_bank;
+    public $card_number;
+    public $card_type;
+    public $expiration_date;
+    public $security_code;
+    public $billing_address;
+    public $fund;
+    public $id_user;
+
+    // constructor
+    public function __construct($id_card_bank, $card_number, $card_type, $expiration_date, $security_code, $billing_address, $fund, $id_user) {
+        $this->id_card_bank = $id_card_bank;
+        $this->card_number = $card_number;
+        $this->card_type = $card_type;
+        $this->expiration_code = $expiration_code;
+        $this->security_code = $security_code;
+        $this->billing_address = $billing_address;
+        $this->fund = $fund;
+        $this->id_user = $id_user;
+    }
+}
+?>
--- views/users/my_account.php
+++ views/users/my_account.php
@@ -14,20 +14,42 @@ if(!isset($_SESSION["email"]))
         justify-content: center;
         align-items:center;
     }
+    .section_my_account h3 {
+        color: #05a3e3;
+        font-weight: 400;
+    }
     .card_account {
         box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
             rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
         border-radius: 10px;
         width: 300px;
         margin: 1rem 1rem;
+        padding: 1rem 1rem;
     }
     .recent_activity {
         display:flex;
         flex-direction: column;
     }
+    .recent_activity div p {
+        margin-top: .2rem;
+    }
+    .recent_activity h4 {
+        margin-bottom: .2rem;
+    }
     .user_options{
         display:flex;
         justify-content: space-around;
+    }
+    .user_option a{
+        display:flex;
+        flex-direction:column;
+        justify-content: center;
+        align-items:center;
+        text-decoration: none;
+        color: black;
+    }
+    .user_option a p{
+        margin-top: .7rem;
     }
     .bank_cards{
         display:flex;
@@ -35,23 +57,35 @@ if(!isset($_SESSION["email"]))
     }
     .bank_cards img{
         width: 50px;
+        margin-right: 1rem;
+    }
+    .link_card {
+        padding: .8rem .8rem;
+        text-decoration:none;
+        color: #05a3e3;
+        border: 1px solid #05a3e3;
+        border-radius: 15px;
     }
 </style>
 
 <section class="section_my_account">
-    <h3>Hello <strong><?= $_SESSION["email"]?></strong></h3>
+    <h3>Hello <?= $_SESSION["email"]?> !</h3>
     <div class="user_options card_account">
-        <div>
-            <span class="material-icons">
-                paid
-            </span>
-            <p>Send</p>
+        <div class="user_option">
+            <a href="#">
+                <span class="material-icons">
+                    paid
+                </span>
+                <p>Send</p>
+            </a>
         </div>
-        <div>
-            <span class="material-icons">
-                payments
-            </span>
-            <p>Request</p>
+        <div class="user_option">
+            <a href="#">
+                <span class="material-icons">
+                    payments
+                </span>
+                <p>Request</p>
+            </a>
         </div>
     </div>
     <div class="recent_activity card_account">
@@ -71,7 +105,7 @@ if(!isset($_SESSION["email"]))
             <p>Debit ****1234</p>
         </div>
     </div>
-    <a href="#">Link a Card</a>
+    <a href="#" class="link_card">Link a Card</a>
 </section>
 
 
